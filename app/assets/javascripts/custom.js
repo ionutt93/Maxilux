0372b4518516bd870dd6dfe9ab9838eb8416cc9b
Ioan Troana <ionutt_93@yahoo.com>
2013-09-05 19:22:11 +0300

    Add scroll animation

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
 	$('.photos').find('.thumbnail').on('click', function() {
-		
 		var nr = $(this).data('nr');
-		alert(nr);
-		$(this).closest(".container").find('.carousel').carousel(nr);
+		var slider = $(this).closest(".container").find('#myCarousel');
+		slider.carousel(nr);
+		
+		$(this).closest("body").animate({
+			'scrollTop': $("#myCarousel").offset().top
+		}, 500);
 	});
 });
