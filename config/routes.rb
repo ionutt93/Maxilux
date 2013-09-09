f3043e77f927334a560fbf376f9bd4b40e058470
Ioan Troana <ionutt_93@yahoo.com>
2013-09-09 19:19:53 +0300

    add new page and sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Website::Application.routes.draw do
   end
 
   resources :articles
+  resources :sessions
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130830145844) do
+ActiveRecord::Schema.define(version: 20130909153413) do
 
   create_table "albums", force: true do |t|
     t.string   "name"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 20130830145844) do
     t.string   "name"
     t.string   "url"
     t.integer  "album_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
